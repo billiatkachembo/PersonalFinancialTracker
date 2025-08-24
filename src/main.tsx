a643c5e3178fc58566ee7765ca8e239e21dc5358
Billiat Kachembo <billiatkachembo1@gmail.com>
2025-08-24 13:12:44 +0200

    Personal Financial Tracker

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
-if (!container) {
-  throw new Error('Root container missing in index.html');
-}
+if (!container) throw new Error('Root container missing in index.html');
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
